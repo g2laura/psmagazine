ac129f74072ef68a7cb05c88d1ab002936a9f3cb
g2laura <laura0703@gmail.com>
2016-05-15 21:47:52 -0500

    Add search icon black. Add content width 1040.

--- wp-content/themes/twentythirteen/header.php
+++ wp-content/themes/twentythirteen/header.php
@@ -25,6 +25,7 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<script src="https://use.fontawesome.com/5e11998d44.js"></script>
+	<link href='https://fonts.googleapis.com/css?family=Fira+Sans' rel='stylesheet' type='text/css'>
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
